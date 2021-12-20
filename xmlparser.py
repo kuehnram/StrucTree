a9790721c24b2b78da6d0a6fe72732cd1145cb3c
kuehnram <kuehnram@users.noreply.github.com>
2021-12-20 13:32:06 +0100

    fix: use utf8

--- xmlparser.py
+++ xmlparser.py
@@ -5,7 +5,7 @@ from model import Entry
 
 def build_model(file_name: str) -> Entry:
     entry = Entry(file_name)
-    with open(file_name, "r") as f:
+    with open(file_name, "r", encoding="utf-8") as f:
         xml_tree = etree.parse(f)
     entry.children.append(convert_xml(xml_tree.getroot()))
 
